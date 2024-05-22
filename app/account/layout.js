368bb7af9206e64814e98c6c44dade4f671f13fa
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-22 17:28:14 +0100

    chore:  update layout

--- app/account/layout.js
+++ app/account/layout.js
@@ -3,11 +3,7 @@ import SideNavigation from "@/app/_components/SideNavigation";
 export default function AccountLayout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr]  h-full gap-12 ">
-      <div>
-        {" "}
-        <SideNavigation />{" "}
-      </div>
-      <div className="py-2"> {children} </div>
+      <SideNavigation /> <div className="py-2"> {children} </div>
     </div>
   );
 }
