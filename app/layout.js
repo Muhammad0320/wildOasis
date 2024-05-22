23a73d9dfda2a73ae8f9fd608278a6a03ff6e176
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-22 15:10:57 +0100

    chore: update tailwind

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-gray-50 min-h-screen ">
+      <body className="bg-primary-950 text-primary-50 min-h-screen ">
         <header>
           <Logo />
           <Navigation />
