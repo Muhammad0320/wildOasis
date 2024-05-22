dd24edc84585b07dcc4d9ca4de76530df77d6421
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-22 17:26:51 +0100

    chore: update page

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,11 +1,13 @@
 import SideNavigation from "@/app/_components/SideNavigation";
 
 export default function AccountLayout({ children }) {
-  <div className="grid grid-cols-[16rem_1fr] gap-12 h-full">
-    <div>
-      {" "}
-      <SideNavigation />{" "}
+  return (
+    <div className="grid grid-cols-[16rem_1fr]  h-full gap-12 ">
+      <div>
+        {" "}
+        <SideNavigation />{" "}
+      </div>
+      <div className="py-2"> {children} </div>
     </div>
-    <div className="py-2"> {children} </div>
-  </div>;
+  );
 }
