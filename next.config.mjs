aae9fdc517617feead7f86b3d15919efcf537956
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-23 04:54:59 +0100

    chore:  update next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nsqgqzoegdznmeaszbga.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-image/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
