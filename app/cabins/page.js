8770e4026133ea6915f5aa5a4b56cf53fa65e2a1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-23 06:08:44 +0100

    chore: update page js

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,11 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinCards from "@/app/_components/CabinCards";
+import Spinner from "@/app/_components/Spinner";
 
-export default async function Page() {
+export default function Page() {
   // CHANGE
-  const cabins = await getCabins();
-
-  console.log(cabins);
 
   return (
     <div>
@@ -21,13 +21,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinCards />
+      </Suspense>
     </div>
   );
 }
