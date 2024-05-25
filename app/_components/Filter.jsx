a5d11c54df4ea31868061ddedb15c6264b46e301
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-25 05:21:17 +0100

    chore : add reuable button comp

--- app/_components/Filter.jsx
+++ app/_components/Filter.jsx
@@ -7,6 +7,8 @@ function Filter() {
 
   const router = useRouter();
 
+  const activeFilter = searchParams.get("capacity") ?? "all";
+
   const pathname = usePathname();
 
   const onClick = (filter) => {
@@ -47,6 +49,19 @@ function Filter() {
         8 &mdash; 12 guests{" "}
       </button>
     </div>
+  );
+}
+
+function Button({ filter, onClick, activeFilter, children }) {
+  return (
+    <button
+      onClick={() => onClick(filter)}
+      className={`  px-5 py-2 hover:bg-primary-700 ${
+        activeFilter === filter ? "bg-primary-700 text-primary-50" : ""
+      }  `}
+    >
+      {children}
+    </button>
   );
 }
 
