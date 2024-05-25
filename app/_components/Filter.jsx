aab98439d44cddf493948a9f74022d442957732d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-25 05:06:23 +0100

    chore: updaet filter

--- app/_components/Filter.jsx
+++ app/_components/Filter.jsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const onClcik = (filter) => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("capacity", filter);
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  };
+
   return (
     <div className="border border-primary-800 flex">
       <button className=" px-5 py-2 hover:bg-primary-700 "> All cabins </button>
