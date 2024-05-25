60da5a492d511055cd7305b587a38860a8ac4159
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-25 05:09:56 +0100

    chore: upfate filte

--- app/_components/Filter.jsx
+++ app/_components/Filter.jsx
@@ -9,7 +9,7 @@ function Filter() {
 
   const pathname = usePathname();
 
-  const onClcik = (filter) => {
+  const onClick = (filter) => {
     const params = new URLSearchParams(searchParams);
 
     params.set("capacity", filter);
@@ -18,16 +18,31 @@ function Filter() {
 
   return (
     <div className="border border-primary-800 flex">
-      <button className=" px-5 py-2 hover:bg-primary-700 "> All cabins </button>
-      <button className=" px-5 py-2 hover:bg-primary-700 ">
+      <button
+        className=" px-5 py-2 hover:bg-primary-700"
+        onClick={() => onClick("all")}
+      >
+        {" "}
+        All cabins{" "}
+      </button>
+      <button
+        className=" px-5 py-2 hover:bg-primary-700 "
+        onClick={() => onClick("small")}
+      >
         {" "}
         1 &mdash; 3 guests{" "}
       </button>
-      <button className=" px-5 py-2 hover:bg-primary-700 ">
+      <button
+        className=" px-5 py-2 hover:bg-primary-700 "
+        onClick={() => onClick("medium")}
+      >
         {" "}
         4 &mdash; 7 guests{" "}
       </button>
-      <button className=" px-5 py-2 hover:bg-primary-700 ">
+      <button
+        className=" px-5 py-2 hover:bg-primary-700 "
+        onClick={() => onClick("large")}
+      >
         {" "}
         8 &mdash; 12 guests{" "}
       </button>
