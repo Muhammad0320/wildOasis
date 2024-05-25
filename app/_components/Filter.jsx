15a2f42753fddce8f17ba995ebad22ab49e11c8d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-25 05:24:18 +0100

    chore:  update filter

--- app/_components/Filter.jsx
+++ app/_components/Filter.jsx
@@ -20,7 +20,7 @@ function Filter() {
 
   return (
     <div className="border border-primary-800 flex">
-      <button
+      {/* <button
         className=" px-5 py-2 hover:bg-primary-700"
         onClick={() => onClick("all")}
       >
@@ -40,14 +40,24 @@ function Filter() {
       >
         {" "}
         4 &mdash; 7 guests{" "}
-      </button>
-      <button
-        className=" px-5 py-2 hover:bg-primary-700 "
-        onClick={() => onClick("large")}
-      >
+      </button> */}
+
+      <Button filter="large" activeFilter={activeFilter} onClick={onClick}>
+        {" "}
+        All cabins{" "}
+      </Button>
+      <Button filter="large" activeFilter={activeFilter} onClick={onClick}>
+        {" "}
+        2 &mdash; 3 guests{" "}
+      </Button>
+      <Button filter="large" activeFilter={activeFilter} onClick={onClick}>
+        {" "}
+        4 &mdash; 7{" "}
+      </Button>
+      <Button filter="large" activeFilter={activeFilter} onClick={onClick}>
         {" "}
         8 &mdash; 12 guests{" "}
-      </button>
+      </Button>
     </div>
   );
 }
