8c66f81b9a86e016c9e2e40b31bb6b89e81b86a2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-27 04:54:51 +0100

    chore: update auth js

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -9,6 +9,12 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
--- middleware.js
+++ middleware.js
@@ -1,8 +1,10 @@
-import { NextResponse } from "next/server";
+import { auth } from "./app/_lib/auth";
 
 // export const middlware = (request) => {
 //   return NextResponse.redirect(new URL("/about", request.url));
 // };
+
+export const middleware = auth;
 
 export const config = {
   mathcer: ["/account"],
