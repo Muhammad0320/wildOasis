5278dfcc81f72096ea141a9a8427a12bf88310e1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-27 04:51:19 +0100

    chore: add middleware

---
+++ middleware.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+// export const middlware = (request) => {
+//   return NextResponse.redirect(new URL("/about", request.url));
+// };
+
+export const config = {
+  mathcer: ["/account"],
+};
