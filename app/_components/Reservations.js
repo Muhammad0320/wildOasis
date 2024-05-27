b30f1294a514873835fac7e3aca20da2a778d04b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-27 04:33:43 +0100

    chore: updated reservations

--- app/_components/Reservations.js
+++ app/_components/Reservations.js
@@ -1,14 +1,16 @@
-import { Suspense } from "react";
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
-import Spinner from "./Spinner";
 
 const Reservations = async ({ cabin }) => {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 min-h-[400px] border border-primary-800">
@@ -17,9 +19,12 @@ const Reservations = async ({ cabin }) => {
         bookedDated={bookedDates}
         cabin={cabin}
       />
-      <Suspense fallback={<Spinner />}>
-        <ReservationForm cabin={cabin} />
-      </Suspense>
+
+      {session.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 };
