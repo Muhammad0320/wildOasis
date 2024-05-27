04f8afa4fcfb50aaa0cc7f85f589a15a4685ea30
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-27 04:08:22 +0100

    chore: update route js

--- app/api/auth/[...nextAuth]/route.js
+++ app/api/auth/[...nextAuth]/route.js
@@ -1,17 +1,1 @@
-import NextAuth from "next-auth";
-
-import Google from "next-auth/providers/google";
-
-const authConfig = {
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-};
-
-export const {
-  auth,
-  handlers: { GET, POST },
-} = NextAuth(authConfig);
+export { GET } from "@/app/_lib/auth";
