a647789ad65b29ba2d86ef8c1b4eb13e701e63b2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-27 04:25:13 +0100

    chore:  update cabin page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,8 @@
 import Cabin from "@/app/_components/Cabin";
 import Reservations from "@/app/_components/Reservations";
+import Spinner from "@/app/_components/Spinner";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
   const cabin = await getCabin(params.cabinId);
@@ -26,13 +28,15 @@ export default async function Page({ params }) {
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <Cabin cabin={cabin} />
-      <div>
-        <h2 className="text-5xl font-semibold text-center text-accent-400 mb-10 ">
-          Reserve {cabin.name} today. Pay on arrival.
-        </h2>
-      </div>
 
-      <Reservations cabin={cabin} />
+      <Suspense fallback={<Spinner />}>
+        <div>
+          <h2 className="text-5xl font-semibold text-center text-accent-400 mb-10 ">
+            Reserve {cabin.name} today. Pay on arrival.
+          </h2>
+        </div>
+        <Reservations cabin={cabin} />
+      </Suspense>
     </div>
   );
 }
