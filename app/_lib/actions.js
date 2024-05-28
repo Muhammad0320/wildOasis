1b216dd210ef7bc39ec4943809022c825ba4d80e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-28 05:05:07 +0100

    chore: update update profilr

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,6 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
 
 export const signinAction = async () => {
   await signIn("google", { redirectTo: "/account" });
@@ -8,4 +8,19 @@ export const signinAction = async () => {
 
 export const signoutAction = async () => {
   await signOut({ redirect: "/" });
+};
+
+export const updateFunction = async (formData) => {
+  const session = await auth();
+
+  if (!session) throw new Error(" You must be logged in ");
+
+  const nationalID = formData.get("nationalID");
+
+  const [nationality, countryFlag] = formData.get("nationaity").split("%");
+  const regex = /^[a-zA-Z0-9]{6,12}$/;
+
+  if (!regex.test(nationalID)) {
+    throw new Error("Please provide a valid national id");
+  }
 };
