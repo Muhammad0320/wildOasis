5e15b1155888d7271031efa0aa84f0a110042b43
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-29 06:26:19 +0100

    chore:  update review card

--- app/_components/ReservationsList.js
+++ app/_components/ReservationsList.js
@@ -1,12 +1,21 @@
 "use client";
 
+import { deleteReservation } from "../_lib/actions";
 import ReservationCard from "./ReservationCard";
 
 export default function ReservationList({ bookings }) {
+  const handleDelete = (bookingId) => {
+    deleteReservation(bookingId);
+  };
+
   return (
     <ul className="space-y-6">
       {bookings.map((booking) => (
-        <ReservationCard booking={booking} key={booking.id} />
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
       ))}
     </ul>
   );
