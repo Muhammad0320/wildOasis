770f81c3809a4152d8976bef3abec969618009a8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-29 06:23:44 +0100

    chore: add reservation list

---
+++ app/_components/ReservationsList.js
@@ -0,0 +1,13 @@
+"use client";
+
+import ReservationCard from "./ReservationCard";
+
+export default function ReservationList({ bookings }) {
+  return (
+    <ul className="space-y-6">
+      {bookings.map((booking) => (
+        <ReservationCard booking={booking} key={booking.id} />
+      ))}
+    </ul>
+  );
+}
